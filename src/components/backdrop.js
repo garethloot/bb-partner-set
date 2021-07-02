bf9051197a91527295abc20af20b2e8f25c3341b
garethloot <32203526+garethloot@users.noreply.github.com>
2021-07-02 13:41:28 +0200

    Step 4 Options and styling

--- src/components/backdrop.js
+++ src/components/backdrop.js
@@ -6,28 +6,47 @@
   jsx: (() => {
     const { Backdrop, CircularProgress } = window.MaterialUI.Core;
     const { env } = B;
+    const { spinnerSize, spinnerThickness } = options;
     const isDev = env === 'dev';
 
     return isDev ? (
-      <div>Backdrop</div>
+      <div className={classes.preview}>
+        <CircularProgress
+          className={classes.progress}
+          size={spinnerSize}
+          thickness={spinnerThickness}
+        />
+      </div>
     ) : (
       <Backdrop className={classes.backdrop} open>
-        <CircularProgress className={classes.progress} />
+        <CircularProgress
+          className={classes.progress}
+          size={spinnerSize}
+          thickness={spinnerThickness}
+        />
       </Backdrop>
     );
   })(),
   styles: B => theme => {
     const style = new B.Styling(theme);
     return {
+      preview: {
+        padding: '20px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        '-webkit-tap-highlight-color': 'transparent',
+      },
       backdrop: {
         '&.MuiBackdrop-root': {
           zIndex: 999,
-          color: '#fff',
         },
       },
       progress: {
         '&.MuiCircularProgress-colorPrimary': {
-          color: '#fff',
+          color: ({ options: { spinnerColor } }) =>
+            style.getColor(spinnerColor),
         },
       },
     };
--- src/prefabs/backdrop.js
+++ src/prefabs/backdrop.js
@@ -5,7 +5,29 @@
   structure: [
     {
       name: 'Backdrop',
-      options: [],
+      options: [
+        {
+          type: 'SIZE',
+          label: 'Spinner size',
+          key: 'spinnerSize',
+          value: '40px',
+          configuration: {
+            as: 'UNIT',
+          },
+        },
+        {
+          type: 'NUMBER',
+          label: 'Spinner thickness',
+          key: 'spinnerThickness',
+          value: 3.6,
+        },
+        {
+          type: 'COLOR',
+          label: 'Spinner color',
+          key: 'spinnerColor',
+          value: 'White',
+        },
+      ],
       descendants: [],
     },
   ],
